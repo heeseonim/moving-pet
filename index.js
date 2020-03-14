f9b9535531796f2c77091fd8fcc398f0863ba9e1
heeseonim <hisundl@naver.com>
2020-03-14 20:56:09 +0900

    #2.7 express core middleware part Two

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => 
+    console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("hello from home");
 
 const handleProfile = (req, res) => res.send("you are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
